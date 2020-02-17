2dd10421671df5c9816f6250199f96a7050c89f8
unknown <mzamturoikhan@gmail.com>
2020-02-17 09:58:10 +0700

    ke-5

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-# list of dict
+# for loop
 
-todos = [
-    {"id": 1, "name": "washing car", "is_done": False},
-    {"id": 2, "name": "helping mom", "is_done": True}
+'''
+for i in range(1, 10):
+    print(i)
+'''
+
+'''
+phone = ['xiaomi', 'iphone6', 'sumsang']
+
+for phone in phone:
+    print(phone)
+'''
+
+phone = [
+    {"id": 1, "name": "xiaomi"},
+    {"id": 2, "name": "iphone6"},
+    {"id": 3, "name": "summsang"},
 ]
 
-print(todos[1]['name'])
+for phone in phone:
+   print(phone['name'])
